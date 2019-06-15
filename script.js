3735a59a7e9c150f4d7f1400b6892fd0a566736b
Ivan Lee <ilee.code@gmail.com>
2019-06-15 10:56:55 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+import { check } from "k6";
+import http from "k6/http";
+
+
+export  let options = {
+  vus:225,        //virtual users
+  rps:225,       //request per second
+  duration:"3m"    //duration of the test
+}
+
+export default function() {
+  var id = Math.ceil(Math.random() * 10000000);
+    let res = http.get(`http://localhost:4000/api/restaurants/${id}/reviews`); //send get request to proxy server 
+    check(res, {
+        "is status 200": (r) => r.status === 200,
+        "transaction time ok": (r) => r.timings.duration < 2000,
+    });
+};
+
+//original module server must be opened as well as the proxy server
---
+++ server/index.js
@@ -0,0 +1,54 @@
+require('newrelic')
+const express = require('express');
+const CORS = require('cors');
+const app  = express();
+const path = require('path');
+const request = require('request');
+
+
+app.use(CORS());
+app.use(express.static(`${__dirname}/../client/dist`));
+
+app.get('/restaurants/:id', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/dist/index.html'))
+})
+
+// // Eugenia's module: (3000)
+// app.get('/restaurants/:id/photos', (req, res) => {
+//   // extract the id with request.params.id
+//   var id = req.params.id
+//   res.redirect(`http://localhost:3000/api/restaurants/${id}/photos`);
+// });
+
+// My module: (port 3001)
+app.get('/api/restaurants/:id/reviews', (req, res) => { //proxy server url
+  var id = req.params.id
+  // console.log(id)
+  request(`http://localhost:3001/api/restaurants/${id}/reviews`, (err,results) => {
+    if(err){
+      res.send(err)
+    }
+  })
+  .pipe(res) //original module url
+});
+
+
+// // Rafe's module: (port 3002)
+// app.get('/restaurants/:id/info/', (req, res) => {
+//   var id = req.params.i
+//   res.redirect(`http://localhost:3002/api/restaurants/${id}/info`);
+// });
+
+// // Erik's module: (port 3003)
+// app.get('/restaurants/:id/googlereviews/', (req, res) => {
+//   var id = req.params.id
+//   res.redirect(`http://localhost:3003/api/restaurants/${id}/googlereviews`);
+// });
+
+app.listen(4000, () => console.log('Listening on port 4000!'));
+
+
+
+//do my module get request
+//make sure endpoint is same as original modules server endpoint
+//stress test
